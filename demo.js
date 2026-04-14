457ad6b71c3ae36b87dd10ac4adc1941a3e485f7
Sven Eppler <sven.eppler@sodgeit.de>
2026-04-14 15:11:26 +0200

    reset demo

--- demo.js
+++ demo.js
@@ -1,17 +1,10 @@
 function countTo(targetNumber) {
-	if(targetNumber < 0) {
-		console.log("You are incompetend. Negative values not supported!");
-		return;
-	}
-
 	for(let i = 0; i < targetNumber; i++) {
 		console.log(i);
 	}
 }
 
-hallo
+countTo(7);
 
-// countTo(7);
-
-countTo(-100);
+// countTo(-1);
 // countTo("Peter");
